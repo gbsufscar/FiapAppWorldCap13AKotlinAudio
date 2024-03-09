79145bc54a487d56ddc097d58ac450c2f76a54e8
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-03-09 12:05:36 -0300

    Código concluído com a introdução dos comandos play, pouse e stop.

--- app/src/main/java/br/com/fiap/audioplayer/MainActivity.kt
+++ app/src/main/java/br/com/fiap/audioplayer/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AudioPlayer(this)
+                    AudioPlayer(this) // Passa o contexto da aplicação para o componente AudioPlayer.
                 }
             }
         }
@@ -41,7 +41,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AudioPlayer(context: Context) {
-
+    /*
+    Variável de estado que vai utilizar um componente chamado MediaPlayer
+    capaz de reproduzir arquivos de áudio.
+    O MediaPlayer precisa do contexto da aplicação e da mídia (áudio) para reproduzir.
+     */
     var player = remember {
         mutableStateOf(MediaPlayer.create(context, R.raw.sem_mundial))
     }
@@ -49,21 +53,38 @@ fun AudioPlayer(context: Context) {
     Box(contentAlignment = Alignment.Center) {
         Row() {
 
-            IconButton(onClick = { }) {
+            IconButton(onClick = {
+                /*
+                Primeiro, se a variável player.value for nula, então instancie um objeto
+                MediaPlayer com o áudio escolhido.
+                Em seguida, chame o método start() para iniciar a reprodução do áudio.
+                 */
+                if (player.value == null){
+                    player.value = MediaPlayer.create(context, R.raw.sem_mundial) // inicializa o MediaPlayer
+                }
+                player.value.start() // Inicia a reprodução do áudio
+            }) {
                 Icon(
                     painter = painterResource(id = R.drawable.play),
                     contentDescription = ""
                 )
             }
 
-            IconButton(onClick = { }) {
+            IconButton(onClick = {
+                player.value.pause() // Pausa a reprodução do áudio
+            }) {
                 Icon(
                     painter = painterResource(id = R.drawable.pause),
                     contentDescription = ""
                 )
             }
 
-            IconButton(onClick = { }) {
+            IconButton(onClick = {
+                player.value.stop() // Para a reprodução do áudio
+                player.value.reset() // Reseta o MediaPlayer para voltar ao estado inicial
+                player.value.release() // Libera os recursos do MediaPlayer (zera a mídia)
+                player.value = null // Atribui null à variável player.value
+            }) {
                 Icon(
                     painter = painterResource(id = R.drawable.stop),
                     contentDescription = ""
